ba873f063c5530067a4e3239c0e794e5fd4cc419
agreeablegrey <andblair2@gmail.com>
2021-01-31 10:04:58 -0500

    Added approach description

--- src/components/pages/contours-vis.js
+++ src/components/pages/contours-vis.js
@@ -50,6 +50,21 @@ const ContoursVis = () => {
             <p className='text-center mt-3'>An interactive contour map of Mount Alyeska and surrounding areas. Work in progress.</p>
         </div>
       </div>
+      <div className='row'>
+        <div className='col'>
+            <h2 className='text-center mt-3'>Approach</h2>
+            <p>
+              I wanted to try using the <a href="https://github.com/d3/d3-contour">contour</a> API provided in D3.js.
+              From my visits to Alaska, I thought the area around Mount Alyeska would be interesting to represent on a contour map since 
+              there is a big difference in elevation between the mud flats of Turnagain Arm and the surrounding Chugach Mountains.
+            </p>
+            <p>
+              I downloaded the Digital Terrain Model (DTM) IFSAR data available at <a href="https://elevation.alaska.gov/">elevation.alaska.gov</a> for the area around Mount Alyeska.
+              Using the <a href="https://rasterio.readthedocs.io/en/latest/intro.html">rasterio</a> Python library I converted the values in the DTM .tif file to JSON and downscaled the elevation data from each datapoint 
+              representing an area of 5 meters to representing an area of 15 meters.
+            </p>
+        </div>
+      </div>
     </Container>
   );
 }
